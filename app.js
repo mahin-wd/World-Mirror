8fcf53dce1ea2cb5bafabcd3506186a39af3304e
mahin-wd <wdmahin.mhm@gmail.com>
2022-09-03 10:34:42 +0600

    dynamic page done

--- app.js
+++ app.js
@@ -1,53 +1,17 @@
-const questions = isClicked =>{
-    const accordion = document.getElementById('accordion');
-    if(isClicked){
-        accordion.classList.remove('d-none')
-    }
-}
-
-const viewQuestions = () =>{
-    questions(true);
-}
-
-const loadNewsMenu = () =>{
-    const  url = `https://openapi.programming-hero.com/api/news/categories`;
-    fetch(url)
+//Dynamic News load
+const dynamicNewsLoad = () =>{
+    fetch('https://openapi.programming-hero.com/api/news/category/04')
     .then(res => res.json())
-    .then(data => displayMenu(data.data.news_category))
-}
-
-const displayMenu = menus =>{
-    const newsMenu = document.getElementById('navbarSupportedContent');
-    menus.forEach(menu=>{ 
-        const menuLis = document.createElement('ul');
-        menuLis.innerHTML = `
-            <li onclick="loadNews('${menu.category_id}')">${menu.category_name}</li>
-        `;
-        menuLis.classList.add('menu-list')
-        newsMenu.appendChild(menuLis);
-    })
-    
-}
-
-const loadNews = (category_id) =>{
-    // loader(true);
-    const url2 = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
-    fetch(url2)
-    .then(res => res.json())
-    .then(data => displayNews(data.data))
+    .then(data => onPageNews(data.data))
     .catch(error => console.log(error))
 }
 
-const displayNews = allNews =>{
+//Dynamic News display
+const onPageNews = dynamicNews =>{
+
     const cardBody = document.getElementById('card-body');
     cardBody.innerHTML = ''
-    if(allNews.length >= 0){
-        const newsNumber = document.getElementById('news-Number');
-        newsNumber.innerHTML = `
-                    <h3>${allNews.length} Items found for  this category</h3>
-                `
-    }
-    allNews.forEach(news =>{
+    dynamicNews.forEach(news =>{
         const cardDiv = document.createElement('div');
         cardDiv.classList.add('col');
         cardDiv.innerHTML = `
@@ -71,12 +35,109 @@ const displayNews = allNews =>{
         </div>
       </div>
         `;
+
         cardBody.appendChild(cardDiv);
     })
-    // loader(false);
+
 }
 
+
+//Blog Questions
+const questions = isClicked =>{
+
+    const accordion = document.getElementById('accordion');
+    if(isClicked){
+        accordion.classList.remove('d-none')
+    }
+}
+
+const viewQuestions = () =>{
+
+    questions(true);
+}
+
+
+
+//Load Menu Part
+const loadNewsMenu = () =>{
+    const  url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMenu(data.data.news_category))
+    .catch(error => console.log(error))
+}
+
+//Display Menu Part
+const displayMenu = menus =>{
+    const newsMenu = document.getElementById('navbarSupportedContent');
+    menus.forEach(menu=>{ 
+        const menuLis = document.createElement('ul');
+        menuLis.innerHTML = `
+            <li onclick="loadNews('${menu.category_id}')">${menu.category_name}</li>
+        `;
+        menuLis.classList.add('menu-list')
+        newsMenu.appendChild(menuLis);
+    })
+    
+}
+
+
+//Load News Part
+const loadNews = (category_id) =>{
+    const url2 = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    fetch(url2)
+    .then(res => res.json())
+    .then(data => displayNews(data.data))
+    .catch(error => console.log(error))
+}
+
+//Display News Part
+const displayNews = allNews =>{
+    const cardBody = document.getElementById('card-body');
+    cardBody.innerHTML = ''
+    if(allNews.length >= 0){
+        const newsNumber = document.getElementById('news-Number');
+        newsNumber.innerHTML = `
+                    <h3>${allNews.length} Items found for  this category</h3>
+                `
+    }
+    
+    allNews.forEach(news =>{
+
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('col');
+        cardDiv.innerHTML = `
+        <div class="card">
+        <img src="${news.image_url}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title mt-3">${news.title.slice(0, 50)}...</h5>
+          <p class="card-text">${news.details.slice(0, 200)}...</p>
+        </div>
+        <div class="card-body">
+            <p>
+                <img class="author-image" src="${news.author.img}">
+                Authore Name: ${news.author.name} <br>
+                Publish Date: ${news.author.published_date}
+            </p>
+
+            <p><i class="fa-light fa-eye"></i>
+            Total View: ${news.total_view}
+            </p>
+            <button class="btn btn-primary" onclick="detail('${news._id}')" data-bs-toggle="modal" data-bs-target="#newsDetaill">Detail</button>
+        </div>
+      </div>
+        `;
+
+        cardBody.appendChild(cardDiv);
+    })
+    
+}
+
+
+
+//Detail on Modal Part
 const detail = (news_id) =>{
+
     const url3 = `https://openapi.programming-hero.com/api/news/${news_id}`
     fetch(url3)
     .then(res => res.json())
@@ -84,7 +145,9 @@ const detail = (news_id) =>{
     .catch(error => console.log(error))
 }
 
+//Display Detail on Modal Part
 const displayModal = newsDetail =>{
+
     newsDetail.forEach(news =>{
         const modalTitle = document.getElementById('newsDetaillLabel');
         modalTitle.innerText = news.title;
@@ -95,13 +158,7 @@ const displayModal = newsDetail =>{
     })
 }
 
-const loader = isLoading =>{
-    const spinner = document.getElementById('spinner');
-    if(isLoading){
-      spinner.classList.remove('d-none');
-    }
-  }
-
+dynamicNewsLoad()
 loadNewsMenu();
 
 
