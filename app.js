b35123f1fc3cc5772f70c74f5c232f0670fd2bc2
mahin-wd <wdmahin.mhm@gmail.com>
2022-09-03 06:46:26 +0600

    news body finished

---
+++ app.js
@@ -0,0 +1,71 @@
+const loadNewsMenu = () =>{
+    const  url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMenu(data.data.news_category))
+}
+
+const displayMenu = menus =>{
+    const newsMenu = document.getElementById('news-menu');
+    menus.forEach(menu=>{ 
+        const menuLis = document.createElement('ul');
+        menuLis.innerHTML = `
+            <li onclick="loadNews('${menu.category_id}')">${menu.category_name}</li>
+        `;
+        menuLis.classList.add('menu-list')
+        newsMenu.appendChild(menuLis);
+    })
+}
+
+const loadNews = (category_id) =>{
+    const url2 = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    fetch(url2)
+    .then(res => res.json())
+    .then(data => displayNews(data.data))
+}
+
+const displayNews = allNews =>{
+    console.log(allNews)
+    const cardBody = document.getElementById('card-body');
+    cardBody.innerHTML = ''
+    allNews.forEach(news =>{
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('col');
+        cardDiv.innerHTML = `
+        <div class="card">
+        <img src="${news.image_url}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title mt-3">${news.title.slice(0, 50)}...</h5>
+          <p class="card-text">${news.details.slice(0, 200)}...</p>
+        </div>
+        <div class="card-body">
+            <p>
+                <img class="author-image" src="${news.author.img}">
+                Authore Name: ${news.author.name} <br>
+                Publish Date: ${news.author.published_date}
+            </p>
+
+            <p><i class="fa-light fa-eye"></i>
+            Total View: ${news.total_view}
+            </p>
+            <button class="btn btn-primary" onclick="detail('${news._id}')" data-bs-toggle="modal" data-bs-target="#newsDetaill">Detail</button>
+        </div>
+      </div>
+        `;
+        cardBody.appendChild(cardDiv);
+    })
+}
+
+const detail = (news_id) =>{
+    const url3 = `https://openapi.programming-hero.com/api/news/${news_id}`
+    fetch(url3)
+    .then(res => res.json())
+    .then(data => console.log(data.data))
+}
+
+const displayModal = () =>{
+    const modalBody = document.getElementById('')
+}
+
+
+loadNewsMenu();
